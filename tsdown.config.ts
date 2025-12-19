18c35680006e571292273197a183424c618c21bf
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-12-19 17:05:42 +0900

    chore: fix tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["src/index.ts", "src/runtime.ts"],
   format: ["esm"],
+  fixedExtension: false,
   dts: {
     sourcemap: process.argv.slice(2).includes("--sourcemap"),
   },
